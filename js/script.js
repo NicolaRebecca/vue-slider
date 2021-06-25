3cf1f642ee5dd65b3dc69d1f097ca65ec6d71ed6
NicolaRebecca <master9895@gmail.com>
2021-06-25 15:16:01 +0200

    es completo. css da sistemare

---
+++ js/script.js
@@ -0,0 +1,45 @@
+new Vue(
+    {
+        el: '#app',
+        data:{
+            photos: [
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+            ],
+            pictureIndex: 0,
+        },
+        created(){
+            setInterval(() => {
+                this.next();
+            }, 3000);
+        },
+        methods: {
+            next: function(){
+                if(this.pictureIndex === (this.photos.length - 1)){
+                    this.pictureIndex = 0;
+                } else{
+                    this.pictureIndex++;
+                }
+            },
+            prev: function(){
+                if (this.pictureIndex = 0) {
+                    this.pictureIndex === this.photos.length - 1;
+                } else {
+                    this.pictureIndex--;
+                }
+            },
+            moveTo: function(index){
+                this.pictureIndex = index;
+            },
+            isCurrentDot: function (index){
+                if (index === this.pictureIndex){
+                    return 'current';
+                } else{
+                    return '';
+                }
+            }
+        }
+    }
+)
